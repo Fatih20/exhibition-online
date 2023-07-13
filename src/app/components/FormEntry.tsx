8dfed9bd020b9f7f78912499687cbae31e4492fe
Fatih20 <fnri39@protonmail.com>
2023-07-13 11:04:37 +0700

    Initial work on homepage.

---
+++ src/app/components/FormEntry.tsx
@@ -0,0 +1,68 @@
+import React, { useState } from "react";
+
+function FormEntry() {
+  const [name, setName] = useState("");
+  const [origin, setOrigin] = useState("");
+  const [knowFrom, setKnowFrom] = useState("");
+  return (
+    <form
+      onSubmit={() => null}
+      className="pointer-events-auto flex flex-col items-center justify-start"
+    >
+      <p className="font-junicode text-2xl text-center mb-6">
+        <strong>
+          Selamat <em>datang!</em>
+        </strong>
+        <br />
+        silakan isi buku tamu terlebih dahulu.
+      </p>
+      <label
+        htmlFor="name-input"
+        className="self-start font-junicode text-base leading-none"
+      >
+        <strong>Nama :</strong>
+      </label>
+      <input
+        type="text"
+        id="name-input"
+        className="w-[600px] self-start mb-2 text-black px-2 py-1"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <label
+        htmlFor="origin-input"
+        className="self-start font-junicode text-base leading-none"
+      >
+        <strong>Asal :</strong>
+      </label>
+      <input
+        type="text"
+        id="origin-input"
+        className="w-[600px] self-start mb-2 text-black px-2 py-1"
+        value={origin}
+        onChange={(e) => setOrigin(e.target.value)}
+      />
+      <label
+        htmlFor="know-from-input"
+        className="self-start font-junicode text-base leading-none"
+      >
+        <strong>Tahu pameran ini dari mana?</strong>
+      </label>
+      <input
+        type="text"
+        id="know-from-input"
+        className="w-[600px] self-start mb-2 text-black px-2 py-1"
+        value={knowFrom}
+        onChange={(e) => setKnowFrom(e.target.value)}
+      />
+      <button
+        type="submit"
+        className="self-end underline decoration-white-ivory underline-offset-1 decoration-1"
+      >
+        SUBMIT
+      </button>
+    </form>
+  );
+}
+
+export default FormEntry;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,58 @@
+"use client";
+
 import Image from "next/image";
+import { useState } from "react";
+import FormEntry from "./components/FormEntry";
 
 export default function Home() {
+  const [isWelcome, setIsWelcome] = useState(false);
+  const [fillingForm, setFillingForm] = useState(false);
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <p className="text-2xl ">Control</p>
-      <p className="text-2xl font-ed-mentasta">Ed Mentasta</p>
-      <p className="text-2xl font-zmg">Zen Maru Gothic</p>
-      <p className="text-2xl font-zmg font-bold">Zen Maru Gothic, Bold</p>
-      <p className="text-2xl font-zmg italic">Zen Maru Gothic, Italic</p>
-      <p className="text-2xl font-zmg font-bold italic">
-        Zen Maru Gothic, Bold Italic
-      </p>
-      <p className="text-2xl font-junicode">Junicode</p>
-      <p className="text-2xl font-junicode font-bold">Junicode, Bold</p>
-      <p className="text-2xl font-junicode italic">Junicode, Italic</p>
-      <p className="text-2xl font-junicode font-bold italic">
-        Junicode, Bold Italic
-      </p>
-
-      <p className="text-2xl font-junicode-condensed">Junicode Condensed</p>
-      <p className="text-2xl font-junicode-condensed font-bold">
-        Junicode Condensed, Bold
-      </p>
-      <p className="text-2xl font-junicode-condensed italic">
-        Junicode Condensed, Italic
-      </p>
-      <p className="text-2xl font-junicode-condensed font-bold italic">
-        Junicode Condensed, Bold Italic
-      </p>
+    <main className="relative flex h-screen bg-white-ivory flex-col items-end justify-start pr-24 pt-32">
+      <div className="absolute flex flex-col inset-0 justify-center items-center z-50 pointer-events-none">
+        <div
+          className={`bg-red w-[768px] p-4 box-border backdrop-blur-[100px] bg-opacity-[0.45] flex flex-col items-center justify-start pointer-events-auto ${
+            fillingForm ? "scale-100 opacity-100" : "scale-0 opacity-0"
+          } transition-all`}
+        >
+          <button className="self-end" onClick={() => setFillingForm(false)}>
+            <Image
+              width={21}
+              height={21}
+              src={"/images/home/x.png"}
+              alt="Close the form"
+            />
+          </button>
+          <FormEntry />
+        </div>
+      </div>
+      {/* Text in the screen */}
+      <div className="relative z-20 flex flex-col items-end justify-start text-red">
+        <h1 className=" font-junicode text-9xl text-right">
+          asa, <br /> cerita,
+          <br /> <em className="font-ed-mentasta">W </em>anita
+        </h1>
+        <p className="w-80 font-zmg text-lg text-justify leading-5">
+          sebuah usaha mengenal bagaimana wanita Indonesia dapat memaknai
+          pilihan hidup mereka masing-masing melalui pameran.
+        </p>
+        <p className="text-lg font-zmg leading-5 text-right">
+          - Astrid N.H. dan Tim
+        </p>
+        <div className="mt-11">
+          <button
+            className="px-11 pt-2 pb-1 text-3xl bg-white-ivory text-red border-red border-2 font-junicode hover:bg-red hover:text-white-ivory"
+            onClick={() => {
+              setIsWelcome(true);
+              setFillingForm(true);
+            }}
+          >
+            <strong>
+              <em>{isWelcome ? "WELCOME" : "START"}</em>
+            </strong>
+          </button>
+        </div>
+      </div>
     </main>
   );
 }

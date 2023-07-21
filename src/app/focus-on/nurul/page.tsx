ba71283c0a868faafbfff3577c364abc72840517
Fatih20 <fnri39@protonmail.com>
2023-07-21 18:50:18 +0700

    6/10 page speakers done.

--- src/app/focus-on/nurul/page.tsx
+++ src/app/focus-on/nurul/page.tsx
@@ -76,6 +76,173 @@ function Page() {
             />
           </div>
         </section>
+        <section className="bg-black space-x-4 grid-system-container p-5">
+          <div className="row-start-2 col-start-1 col-span-3 flex flex-col items-start justify-start gap-5 text-justify">
+            <p className="text-sm">
+              Lulus S1 Kimia Murni di Malang, ia berefleksi.
+              <span className="font-junicode text-base">
+                “Selepas lulus dari jenjang S-1, sebetulnya saya tidak tahu mau
+                ngapain saat kembali ke Sumbawa. Di Sumbawa, tidak ada banyak
+                contoh figur orang-orang yang ke luar negeri. Namun, lingkungan
+                baru yang memfasilitasi untuk bebas bereksplorasi membuat saya
+                semakin enjoy nge-lab sebagai therapeutic and drugs analyst di
+                Balepom. Ditambah dengan berburu informasi melalui Campus Expo
+                dan beberapa kenalan, saya terpecut untuk mulai meluruskan niat
+                menuntut pendidikan di luar negeri, bukan sekadar candaan sesama
+                teman saja.”
+              </span>
+            </p>
+            <Image
+              src={"/images/focus-on/35.png"}
+              alt="Nurul Izzati in Russia"
+              width={137}
+              height={150}
+            />{" "}
+          </div>
+          <div className="row-start-2 col-start-5 col-span-3 flex flex-col items-start justify-start gap-5 text-justify">
+            <p className="text-sm">
+              Mbak Nurul tumbuh di antara alam sawah dan laut Sumbawa. Ayahnya
+              bekerja sebagai guru SD dan ibunya seorang IRT, menekankan
+              kepentingan agama, attitude, dan kejujuran. Mengenai peran
+              perempuan yang ia lihat sejak kecil, Mbak Nurul menceritakan
+              dinamika yang ia lihat dalam keluarga dan keluarga
+              besarnya. Keluarga besarnya memiliki pandangan yang lebih
+              konservatif mengenai potensi perempuan. Hal ini tidak pernah
+              didengarkan serius oleh bapaknya.
+            </p>
+          </div>
+          <div className="row-start-2 col-start-9 col-span-3 flex flex-col items-center justify-start gap-5 text-justify">
+            <p className="text-sm">
+              <strong>Komitmen Berangkat, Komitmen Kembali</strong>
+              <br />
+              Dengan proses mengejar skor IELTS yang berdarah-darah sembari
+              merawat ibu yang sakit sampai sempat merasa putus asa, Mba Nurul
+              akhirnya bisa yakin untuk tes ketiga kalinya. Ia mantap memilih
+              Lund sebagai tujuan melanjutkan pendidikan tinggi. Melalui bantuan
+              rekan yang pernah belajar di Lund dan founder UTS, komunikasi
+              dengan LPDP berbuah manis dan bertepatan dengan Sumbawa yang
+              membutuhkan SDM yang bisa membantu mengembangkan daerahnya.
+            </p>
+            <Image
+              src={"/images/focus-on/36.png"}
+              alt="Nurul Izzati in Russia"
+              width={188}
+              height={114}
+              className="self-start"
+            />{" "}
+            <p className="font-junicode">
+              “Saya pun sudah menuliskannya bahwa suatu saat nanti saya
+              berkomitmen untuk kembali ke Sumbawa.”
+            </p>
+            <p className="text-sm">
+              Pada intinya, Mba Nurul meyakini dan ini pun menjadi harapannya
+              bagi beliau,
+            </p>
+            <p className="font-junicode">
+              “Jangan pernah melimitasi diri. Tidak ada pilihan yang salah dari
+              berkarier atau bekerja di rumah. Perempuan bisa berkontribusi
+              apapun pilihan mereka.”{" "}
+            </p>
+          </div>
+        </section>
+        <section className="bg-black space-x-4 grid-system-container p-5">
+          <div className="row-start-2 col-start-1 col-span-3 flex flex-col items-start justify-start gap-5 text-justify">
+            <p className="text-sm">
+              <strong>Transformasi melalui Kompetisi</strong>
+              <br />
+              Studi S2 di Lund merupakan sesuatu yang berat bagi Mba Nurul. Dari
+              standar yang tinggi, barrier dalam bahasa, hingga proses akademik
+              di sana, Mba Nurul merasa terdorong untuk berkembang. Terinspirasi
+              untuk mengikuti iGEM karena keberadaan tim dari UTS yang ia
+              ketahui sebelumnya, ia malah terpilih menjadi ketua tim
+              multinasional iGEM Lund. Hal ini berbuah hasil pada saat di sana
+              ia pertama kali mendapatkan pujian dan apresiasi tulus dari kolega
+              yang cenderung tertutup beserta supervisornya.
+              <span className="font-junicode text-base">
+                “Orang Indonesia memang terkenal untuk kerja kerasnya. Di sana,
+                orang Swedia tidak tergerak untuk berkompetisi --semuanya sudah
+                cukup terpenuhi.”.
+              </span>
+            </p>
+            <Image
+              src={"/images/focus-on/37.png"}
+              alt="Nurul Izzati having foods"
+              width={194}
+              height={113}
+              className="self-start"
+            />{" "}
+            <p className="text-sm">
+              Mba Nurul meningkatkan kepercayaan dirinya seiring dengan
+              pencapaian tim untuk meraih emas. Sekarang, ia semakin percaya
+              dengan potensi SDM Indonesia termasuk dirinya, yang ia pikir perlu
+              untuk perlahan mengganti pikiran ‘minder’ saat dibandingkan dengan
+              orang luar.
+            </p>
+          </div>
+          <div className="row-start-2 col-start-5 col-span-3 flex flex-col items-start justify-start gap-5 text-justify">
+            <p className="text-sm">
+              <strong>Hubungan dan Keluarga</strong>
+              <br />
+              Soal keluarga dan hubungan, Mbak Nurul turut tertawa karena
+              kisahnya yang justru lucu.
+              <br />
+              <br />
+              <span className="font-junicode text-base">
+                “Enggak mau sama orang Sumbawa, tapi ternyata jadinya malah sama
+                orang Sumbawa.”
+              </span>
+              Mbak Nurul menyadari kalau ia tidak menyetujui patriarki dan tidak
+              ingin berkeluarga dengan orang patriarkis.
+            </p>
+            <p className="font-junicode">
+              “Waktu S-2 santai aja dan tipikal terbuka. Tiba-tiba, saat balik
+              ke Sumbawa, saya dilamar, tanpa pacaran. Selama pekan-pekan
+              pertama setelah dilamar itu, saya mikir gimana cara nolaknya?
+              Akhirnya, saya bikin pertanyaan-pertanyaan mengenai patriarki dan
+              jawaban dari beliau memuaskan. Saya lihat pros and cons-nya dan
+              memenuhi kriteria semua.”
+            </p>
+            <p className="text-sm">
+              Seiring menjalin hubungan pernikahan pun, justru ia merasakan
+              peran mendidik anak, melakukan pekerjaan rumah, dan bekerja justru
+              bisa bersinergi dengan baik.
+            </p>
+            <Image
+              src={"/images/focus-on/38.png"}
+              alt="Nurul Izzati and Flowers"
+              width={140}
+              height={108}
+              className="self-start"
+            />{" "}
+          </div>
+          <div className="row-start-2 col-start-9 col-span-3 flex flex-col items-center justify-start gap-5 text-justify">
+            <p className="font-junicode">
+              “Saya jadi wakil rektor menikah belum sebulan menjabat. Suami pun
+              menjadi wakil atau dekan. Kita enggak punya babysitter, dan kita
+              bagi jadwal parenting. Justru suami yang meyakinkan bahwa kalau
+              kita sanggup melakukan ini bersama. Di rumah yang sering masak pun
+              suami dan pekerjaan domestik lainnya kita biasanya gantian atau
+              bagi tugas.”{" "}
+            </p>
+            <p className="text-sm">
+              Hal ini pun pelajaran yang dapat dipetik dari negara Skandinavia
+              mengenai parenting style dan life balance.
+            </p>
+            <p className="text-sm">
+              <strong>Kontribusi Kembali</strong>
+              <br />
+              Saat pulang ke Indonesia, Mba Nurul juga menjadi iGEM Ambassador
+              untuk Asia. Dengan kenekatan dan optimisme luar biasa, bersama Mas
+              Farhan (kolaborator), akhirnya ia mendirikan Synbio.
+              <span className="font-junicode text-base">
+                “Ternyata bisa loh, jalan. Ada banyak yang tertarik”.
+              </span>{" "}
+              Dalam 2 tahun, semakin meningkat jumlah peserta dan minat. Mereka
+              juga membentuk paper mengenai potensi synbio komunitas di
+              Indonesia bersama.
+            </p>
+          </div>
+        </section>
 
         <BackToStart
           onBack={() => {
--- src/app/focus-on/wanda/page.tsx
+++ src/app/focus-on/wanda/page.tsx
@@ -78,6 +78,180 @@ function Page() {
             />
           </div>
         </section>
+        <section className="bg-black space-x-4 grid-system-container p-5">
+          <div className="row-start-2 col-start-1 col-span-3 flex flex-col items-start justify-start gap-5 text-justify">
+            <p className="text-sm">
+              Lahir di desa di Jember, Jawa Timur dan dibesarkan bersama adik
+              perempuannya oleh ibunya seorang diri, Kak Wanda memulai cerita
+              dengan merefleksikan peran privelese dalam hidupnya.
+              <br />
+              <br />
+              <span className="font-junicode text-base">
+                “Aku rasa keluargaku lebih beruntung daripada keadaan orang
+                sekitar di Jember, namun saat dibandingkan dengan keadaan dengan
+                orang yang aku temui di S1 dan S2, aku bilang memang keluargaku
+                tidak privelese. Lapisan privelese memang berbeda ya, tergantung
+                pembandingnya.”
+              </span>
+              , ujarnya.
+              <br />
+              <br />
+            </p>
+            <Image
+              src={"/images/focus-on/31.png"}
+              alt="Wanda"
+              width={113}
+              height={101}
+            />{" "}
+            <p className="text-sm">
+              <span className="font-junicode text-base">
+                “Aku selalu diajarkan pastinya, seperti keluarga gender
+                tradisional lainnya, bahwa perempuan harus begini, harus begitu,
+                tidak boleh melakukan itu. Kalau perempuan harus bisa berbersih.
+                Nanti aku akan menikah, aku akan jadi istri, nantinya aku akan
+                merawat anak dan sebagainya.”
+              </span>
+              , kutip Kak Wanda.
+            </p>
+          </div>
+          <div className="row-start-2 col-start-5 col-span-3 flex flex-col items-start justify-start gap-5 text-justify">
+            <p className="text-sm">
+              <strong>Proses Pembentukan Identitas</strong>
+              <br />
+              Tumbuh di lingkungan tradisional dengan pembagian peran gender
+              yang cukup eksplisit, Kak Wanda mengingat pengalaman
+              mempertanyakan hal tersebut. Bermain dengan saudara laki-laki,
+              mengemban label ‘tomboy’, hingga menjadi koordinator futsal tiap
+              hari di SMA, ia tetap melakukan hal yang ia mau. Pemahaman Kak
+              Wanda semakin ketara saat ia mengemban studi lebih lanjut di
+              Kajian Gender dan memperoleh perspektif baru dari lingkungannya.
+              Pembentukan identitas baginya adalah proses menciptakan, tak hanya
+              menerima. 
+            </p>
+            <p className="font-junicode">
+              “Aku tumbuh dengan nilai-nilai yang sangat berbeda dengan apa yang
+              aku percaya sekarang. Dulu ada nilai-nilai yang kita percaya
+              karena kita diajarkan dengan demikian. Jadinya pasti kita percaya
+              bahwa itu hal yang baik, benar, lalu diikuti. Tetapi saat kita
+              punya pengetahuan baru, pengalaman yang berbeda, kedewasaan dan
+              kebijaksanaan yang berbeda, itu membuat norma-norma sebelumnya
+              tidak relevan. Jadi ada perubahan pemahaman dan norma yang kita
+              percaya.  Terbentuk suatu nilai baru dari hasil yang kita dapat,
+              alami, baca, dan pelajari. Dulu ini mungkin relevan buat mereka,
+              tapi enggak relevan buat aku.”  
+            </p>
+          </div>
+          <div className="row-start-2 col-start-9 col-span-3 flex flex-col items-center justify-start gap-5 text-justify">
+            <p className="text-sm">
+              <strong>Memupuk Ilmu dan Kepercayaan Diri</strong>
+              <br />
+              Studi S1 Kak Wanda membutuhkan waktu 7 tahun akibat beberapa
+              alasan. Mencari jalannya sendiri dengan pekerjaan freelance, ia
+              menjadi kontributor penulis regular di Mubadalah, menulis banyak
+              artikel dengan perspektif gender. Mimpinya berbelok. Pada umur 28
+              tahun, ia memilih untuk melanjutkan S2 di UI dengan jurusan Kajian
+              Gender.
+            </p>
+            <p className="font-junicode">
+              “Di Kajian Gender, aku merasa: ternyata aku enggak bodoh. Itu
+              kesimpulan yang mungkin bagi orang lain shallow. Saat itu, self
+              esteem aku sempat hancur. Aku sudah punya pemahaman bahwa diriku
+              ‘bego’. Jadi, pemahaman sesederhana bahwa aku enggak bego itu
+              sesuatu yang besar sekali.”{" "}
+              <span className="font-zmg text-sm">
+                Kak Wanda membuktikan prestasinya dengan meraih IP 4 di semester
+                pertama, lulus dengan gelar summa cum laude, dan menjadi
+                representasi mahasiswa untuk memberikan pidato pada upacara
+                kelulusan. Tak hanya itu, ia melakukan hal tersebut sambil
+                mendirikan dua inisiatif, Puzzle Diri beserta Cerita Kubi.
+              </span>{" "}
+              “Belajar itu menyenangkan. Belajar itu menggairahkan.”
+            </p>
+          </div>
+        </section>
+        <section className="bg-black space-x-4 grid-system-container p-5">
+          <div className="row-start-2 col-start-1 col-span-3 flex flex-col items-start justify-start gap-5 text-justify">
+            <p className="text-sm">
+              Kak Wanda menunjukkan sendiri dengan usaha bagaimana pencarian
+              ilmu tak terbatas suatu timeline tertentu. Dalam prosesnya, ia
+              menemukan kembali rasa percaya dirinya.
+            </p>
+            <Image
+              src={"/images/focus-on/32.png"}
+              alt="Wanda"
+              width={212}
+              height={123}
+            />{" "}
+            <p className="text-sm">
+              <strong>Childfree, Hak Asasi</strong>
+              <br />
+              Diantara pekerjaan lainnya, Kak Wanda juga sedang berusaha untuk
+              membuat proyek thesis-nya menjadi tulisan populer. Karya keduanya
+              berhubungan dengan topik childfree, khususnya voluntary childfree,
+              yaitu keputusan untuk tidak memiliki anak berdasar pilihan bukan
+              karena tidakmampuan (involuntary). Harapannya, buku tersebut dapat
+              menjadi riset groundbreaking dan referensi studi gender pada
+              konteks Indonesia yang masih jarang terlihat.
+            </p>
+          </div>
+          <div className="row-start-2 col-start-5 col-span-3 flex flex-col items-start justify-start gap-5 text-justify">
+            <p className="font-junicode">
+              “Aku ingin childfree ini bisa jadi pemahaman umum, bahwa childfree
+              itu hak, loh. Itu adalah hak asasi. Perempuan punya dan boleh
+              memiliki hak kesehatan seksual dan reproduksi bagi perempuan. Jadi
+              kita bisa saling memahami, saling berempati, tak hanya mengecap
+              bahwa ‘perempuan tidak punya anak itu atau sebagainya’. Dicap
+              stigma dan mitos, padahal cerira perempuan itu berbeda-beda.”
+            </p>
+            <p className="text-sm">
+              Berbasis di Jakarta, Kak Wanda bersemangat dengan visi ia
+              kedepannya.
+            </p>
+            <Image
+              src={"/images/focus-on/33.png"}
+              alt="Wanda Speaking"
+              width={223}
+              height={143}
+            />{" "}
+            <p className="text-sm">
+              <strong>Resolusi Diri</strong>
+              <br />
+              <span className="font-junicode">
+                “Makin kesini, makin jauh berjalan, makin menemukan puzzle diri
+                aku sebagai manusia. Sekarang aku umur 30, aku engga sabar untuk
+                umur 35, dan seterunya. Karena aku yakin kehidupan aku akan jauh
+                lebih seru.”
+              </span>
+            </p>
+          </div>
+
+          <div className="row-start-2 col-start-9 col-span-3 flex flex-col items-center justify-start gap-5 text-justify">
+            <p className="text-sm">
+              Perbedaan tingkat pendidikan dan nilai baru sempat membentuk ‘gap’
+              pemahaman antara Kak Wanda dan keluarga. Kak Wanda menemukan
+              bukunya yang mengeksplorasi peran perempuan tradisional malah
+              menjadi suatu titik resolusi bersama orang tuanya. 
+            </p>
+            <p className="font-junicode">
+              “Awalnya aku juga mikir pada saat mau menerbitkan buku mengenai
+              reaksi keluargaku. Surprisingly, saat aku menerbitkan buku ini,
+              karya ini jadi jawaban bagi mereka. Awalnya mereka mempersalahkan
+              status lajang aku. Pemahaman lajang juga mungkin terbatas,
+              dipahaminya sebagai tidak ingin menikah sama sekali atau jomblo
+              terus. Kasarnya ya tidak laku. Padahal aku pernah punya pasangan,
+              dan untuk lajang terserah kamu mendefinisikan seperti apa. Jadi
+              justru ini jawaban buat orang tua aku, dan mereka bangga aku punya
+              karya. Bahwa lajang itu pilihan.”  
+            </p>
+            <Image
+              src={"/images/focus-on/34.png"}
+              alt="Wanda Demo"
+              width={223}
+              height={143}
+              className="self-start"
+            />{" "}
+          </div>
+        </section>
 
         <BackToStart
           onBack={() => {

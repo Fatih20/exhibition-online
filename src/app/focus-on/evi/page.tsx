56eadd594ea76eff93832054b1829de77059bd10
Fatih20 <fnri39@protonmail.com>
2023-07-22 07:23:50 +0700

    Finish 8/10 speakers page.

--- src/app/focus-on/evi/page.tsx
+++ src/app/focus-on/evi/page.tsx
@@ -36,7 +36,7 @@ function Page() {
       <RedBar progressPercent={percent} />
       <main
         ref={containerRef}
-        className="flex overflow-x-auto overflow-y-hidden items-start flex-nowrap whitespace-nowrap h-full justify-start scrollbar-hide"
+        className="flex overflow-x-auto overflow-y-hidden items-start flex-nowrap whitespace-nowrap h-full justify-start scrollbar-hide font-zmg"
       >
         <section className="bg-black grid-system-container">
           <div className="pl-10 row-start-2 col-span-8 col-start-1 flex flex-col items-start justify-start">
@@ -76,6 +76,266 @@ function Page() {
               height={2597}
               alt="Evi Mariani"
             />
+          </div>
+        </section>
+        <section className="bg-black space-x-4 grid-system-container p-5 ">
+          <div className="row-start-2 col-start-1 col-span-3 flex flex-col items-start justify-start gap-5 text-justify">
+            <p className="text-sm">
+              Bu Evi Mariani lahir di Bandung, tumbuh besar di keluarga
+              Tionghoa-Indonesia yang beragama Katolik taat. Telah bekerja
+              sebagai jurnalis dua dekade, Bu Evi belajar Ilmu Komunikasi di
+              UGM, lalu melanjutkan studinya ke Belanda untuk belajar Studi
+              Urban.
+              <br />
+              <br />
+              Bu Evi, walaupun saat diwawancarai berapi-api membicarakan posisi
+              ideologis, rupanya tidak mendasarkan pilihan karir maupun studinya
+              dari idealisme tertentu. Saat masuk kuliah S1, mimpinya terbilang
+              naif.
+            </p>
+            <p className="font-junicode">
+              “Masuk ke Ilmu Komunikasi UGM, ada konsentrasi jurnalisme jadi
+              memilih itu. Sesederhana alasannya pengen keluar negeri,
+              jalan-jalan, dibayarin orang. Setelahnya, ikut pers mahasiswa dan
+              hidup sebagai mahasiswa ilmu sosial-politik secara keseluruhan
+              menjadi pengalaman formatif hidup.”
+            </p>
+            <Image
+              src={"/images/focus-on/5.png"}
+              alt="Habis Makan Jangan Lupa Minum"
+              width={171}
+              height={120}
+              className="self-start"
+            />{" "}
+          </div>
+          <div className="row-start-2 col-start-5 col-span-3 flex flex-col items-start justify-start gap-5 text-justify">
+            <p className="text-sm">
+              Paska lulus kuliah pada tahun 2001, Bu Evi lalu merantau ke
+              Jakarta. Puluhan CV ia sebarkan ke koran-koran besar, namun
+              berbulan-bulan tidak ada yang memberikan kesempatan wawancara.
+              Selama setengah tahun, ia sempat menjadi penerjemah dahulu.
+              Barulah kemudian, Tempo, Suara Pembaharuan, dan The Jakarta Post
+              menawarkan pekerjaan. Ia memilih untuk bekerja di desk city The
+              Jakarta Post. Pada awal karirnya, ia banyak melaporkan tentang
+              isu-isu rakyat miskin kota, mulai dari kenaikan BBM hingga
+              penggusuran paksa rumah rakyat. Disanalah ia mengaku jatuh cinta.
+            </p>
+            <Image
+              src={"/images/focus-on/6.png"}
+              alt="Habis Makan Jangan Lupa Minum"
+              width={171}
+              height={120}
+              className="self-start"
+            />{" "}
+            <p className="font-junicode">
+              “The measure of a success in journalism itu adalah bisa menelpon
+              pejabat. (Sedangkan) aku gapernah berjabat tangan sama presiden
+              sekalipun. I don’t think I’ve missed much juga. Aku jatuh cinta
+              dengan desk city, akhirnya sempat menjadi kepala desk city pula di
+              The Jakarta Post. S2 juga ambil Studi Urban.”
+            </p>
+          </div>
+          <div className="row-start-2 col-start-9 col-span-3 flex flex-col items-start justify-start gap-5 text-justify">
+            <p className="text-sm">
+              Ketika ditemui untuk sesi wawancara, Bu Evi membawa serta anaknya
+              yang rewel karena sempat sakit. Ia juga membicarakan sedikit
+              tentang bagaimana ia bertemu suaminya. Mereka bertemu saat kuliah
+              tiga dekade lalu, namun baru berpacaran pada tahun 2008.
+              Kedatangannya ke Jogja juga berkaitan dengan urusan suaminya di
+              ArtJog 2023. Suaminya seorang pegiat seni teater yang telah
+              merintis Teater Garasi sejak kuliah.
+            </p>
+            <Image
+              src={"/images/focus-on/7.png"}
+              alt="Lalita Project"
+              width={214}
+              height={75}
+              className="self-start"
+            />{" "}
+            <p className="text-sm">
+              Membicarakan peran perempuan dan dinamika keluarga, Bu Evi
+              berefleksi tentang dampak fenomena floating mass yang dimulai
+              sejak Orde Baru.
+              <br />
+              <br />
+              <span className="font-junicode text-base">
+                “Saat ikut Sekolah Perempuan, aku mikir jauh kalau warisan Orde
+                Baru floating mass ini sudah merasuk ke keluarga. Orang-orang
+                (di dalamnya) dilatih untuk setiap kali bertemu daripada
+                ngomongin politik seperti harga beras naik, efektivitas kartu
+                BPJS, malah ngomongin kapan kawin. Sehingga, percakapan meja
+                makan keluarga (telah) disanitasi menjadi soal masalah moral
+                pribadi, bukan moral publik. Pembicaraan disana bukan lagi soal
+                korupsi dan ketimpangan tapi moral pribadi tetangga (yang entah)
+                LGBTQ atau bukan.”
+              </span>
+            </p>
+          </div>
+        </section>
+        <section className="bg-black space-x-4 grid-system-container p-5">
+          <div className="row-start-2 col-start-1 col-span-3 flex flex-col items-start justify-start gap-5 text-justify">
+            <p className="text-sm">
+              Floating mass adalah proyek rekayasa sosial buatan rezim Orde
+              Baru. Proyek tersebut bertujuan untuk mengubah masyarakat
+              Indonesia menjadi &quot;massa mengambang&quot; yang apatis soal
+              politik partisipatoris demi mendukung misi pembangunan nasional.
+              Melalui floating mass, pemerintah menekan ekspresi identitas
+              politik rakyat serta menyetir agar rakyat memilih fokus kepada
+              kemapanan hidup dan stabilitas ekonomi masing-masing. Bibit-bibit
+              aktivitas politik seperti pengorganisasian keluhan sosial dibabat
+              habis, mulai dari level formal hingga personal.
+              <br />
+              <br />
+              <strong>Bangkitnya Kepekaan Politik saat Kuliah</strong>
+              <br />
+              Bu Evi mengaku betapa terbatas pengetahuan dirinya tentang keadaan
+              sosial politik Indonesia sebelum masuk kuliah. Walaupun ia
+              keturunan Tionghoa, ia tidak pernah mendengar nama Soe Hok Gie
+              satu kali pun saat belia. Ia juga tidak pernah mendengar bahwa
+              setidaknya ada setengah juta orang dibantai saat peristiwa 1965.
+              Hal-hal itu diketahuinya lewat diskusi dan publikasi pers
+              mahasiswa fakultas. Radikalisasi politiknya dimulai sejak pertama
+              kali menginjakkan kaki di UGM.
+            </p>
+          </div>
+          <div className="row-start-2 col-start-5 col-span-3 flex flex-col items-start justify-start gap-5 text-justify">
+            <p className="font-junicode">
+              “Pertama kali masuk kampus langsung dihadapkan sama demonstrasi
+              Tegak 5 untuk hak-hak mahasiswa. Mereka berteriak bahwa kita
+              kuliah dibayar uang rakyat! Aku saat itu hanya melihat berkeliling
+              dan menyadari bahwa aku Cina sendiri diantara banyak orang.”
+            </p>
+            <p className="text-sm">
+              Karir jurnalistik dan tulis-menulis pula ia rintis sejak kuliah.
+              Bu Evi pernah menjadi pemimpin redaksi publikasi mahasiswa Suara
+              dari Korps Mahasiswa Ilmu Komunikasi UGM. Selain itu, ia aktif
+              dalam pembuatan tulisan di pers mahasiswa Sintesa. Bersama
+              Sintesa, ia kemudian ambil peran dalam pembuatan edisi tulisan
+              Indonesia Cemas sebagai respon jargon dirgahayu 50 tahun
+              kemerdekaan RI yaitu Indonesia Emas. Bu Evi juga sempat bercerita
+              dengan tertawa getir tentang pengalamannya meliput demonstrasi
+              hingga dipentung, dikejar, dan ditangkap polisi.
+            </p>
+
+            <p className="font-junicode">
+              “Tapi, terus polisinya ga mood nangkapin. Jadi, aku sama
+              teman-teman diturunin dari mobil polisi.”
+            </p>
+          </div>
+          <div className="row-start-2 col-start-9 col-span-3 flex flex-col items-center justify-start gap-5 text-justify">
+            <p className="text-sm">
+              <strong>Jurnalis atau Aktivis?</strong>
+              <br />
+              Ketika diwawancarai, Bu Evi banyak membicarakan tentang sepak
+              terjangnya di ranah jurnalisme Indonesia. Terutama,
+              konflik-konflik editorial yang ia sebut sebagai momen-momen
+              tergelapnya sebagai jurnalis pada tahun 2015-2016. Bu Evi
+              menjelaskan secara rinci jatuh bangun usahanya untuk
+              mempertahankan prinsip di ruangan dimana prinsip-prinsip tersebut
+              dipertanyakan oleh kolega kerjanya.
+              <br />
+              <br />
+              Bu Evi tahu benar bahwa kolega-koleganya di The Jakarta Post
+              terdiri dari kelompok berpendidikan ibukota yang beruntung. Tipe
+              yang terbiasa memakai bahasa Inggris sejak kecil dan sudah
+              terpapar pendidikan di luar negeri setidaknya sejak kuliah. Ia
+              juga paham kalau The Jakarta Post sangat keukeuh perihal
+              pemberitaan isu-isu HAM dan gender. Tapi, orang-orang tersebut
+              punya sikap yang berbeda soal konflik kelas.
+            </p>
+            <Image
+              src={"/images/focus-on/8.png"}
+              alt="WIUI with Andovi"
+              width={329}
+              height={128}
+              className="self-start"
+            />{" "}
+          </div>
+        </section>
+        <section className="bg-black space-x-4 grid-system-container p-5">
+          <div className="row-start-2 col-start-1 col-span-3 flex flex-col items-start justify-start gap-5 text-justify">
+            <p className="font-junicode">
+              “(Saat pemberitaan penggusuran oleh Ahok di 2015 naik), aku
+              berantem di editorial. (Kolegaku berkata), itu relokasi kan,
+              kenapa kamu sebut forced eviction. Ya, aku bilang forced eviction.
+              Awalnya, aku suka Ahok. Masuk akal kalau dia jadi media darling,
+              misalnya dengan narasi keinginannya membersihkan PNS yang korupsi.
+              Tapi, ketika dia mendzolimi yang lebih lemah dari dia, I don’t
+              think it’s good leadership. Kamu kalau jadi leader kok sama yang
+              lemah malah memfitnah.”
+            </p>
+            <p className="text-sm">
+              Gesekan di ruang editorial ini menggagalkan promosinya untuk
+              menjadi Managing Editor di The Jakarta Post. Mbak Evi kemudian
+              protes ke redaksi pelaksana yang menganggapnya terlalu “aktivis”.
+            </p>
+            <Image
+              src={"/images/focus-on/9.png"}
+              alt=""
+              width={330}
+              height={94}
+              className="self-start"
+            />{" "}
+            <p className="font-junicode">
+              “Aku kaya gini karena aku homegrown Jakarta Post journalist.
+              <em>I respect human rights, gender equality.</em> Ada banyak
+              jurnalis disini yang jadi aktivis di isu-isu tersebut.
+              <em>
+                But, none of them you called an activist, not journalists.
+              </em>{" "}
+              Tapi, kenapa ketika aku defend the poor you call me an activist?”
+            </p>
+          </div>
+          <div className="row-start-2 col-start-5 col-span-3 flex flex-col items-start justify-start gap-5 text-justify">
+            <p className="text-sm">
+              Walaupun sempat meragukan diri, tapi Mbak Evi bersyukur ia teguh
+              dalam pendiriannya.
+            </p>
+            <p className="font-junicode">
+              “(Penggusuran Ahok) merupakan human rights violation. Forced
+              evictionnya tidak jauh berbeda dari zaman Sutiyoso-Fauzi Bowo,
+              berita yang pernah aku liput juga saat masih menjadi cub reporter
+              pada tahun 2003. It’s the same, ga transparan, ga partisipatoris,
+              martabat orang diinjak-injak.”
+            </p>
+
+            <p className="font-junicode">
+              “(Saat pemberitaan penggusuran oleh Ahok di 2015 naik), aku
+              berantem di editorial. (Kolegaku berkata), itu relokasi kan,
+              kenapa kamu sebut forced eviction. Ya, aku bilang forced eviction.
+              Awalnya, aku suka Ahok. Masuk akal kalau dia jadi media darling,
+              misalnya dengan narasi keinginannya membersihkan PNS yang korupsi.
+              Tapi, ketika dia mendzolimi yang lebih lemah dari dia, I don’t
+              think it’s good leadership. Kamu kalau jadi leader kok sama yang
+              lemah malah memfitnah.”
+            </p>
+          </div>
+          <div className="row-start-2 col-start-9 col-span-3 flex flex-col items-center justify-start gap-5 text-justify">
+            <p className="font-junicode">
+              “Perkembangan jurnalistik di Amerika menemukan bahwa yang disebut
+              objektivitas itu ternyata rapuh banget, itu kumpulan
+              subjektivitas. Lalu, kumpulan subjektivitas siapa? Jawabannya ya,
+              orang-orang yang paling berkuasa di newsroom masing-masing. Kalau
+              di Amerika white male cishet. Kalau di Indonesia ya kelas
+              menengah. Mereka yang nuduh aku ga objektif adalah kumpulan
+              subjektivitas kelas menengah yang Jakarta-centric,
+              western-educated, privilege banget, kerja formal, rumah di Bekasi
+              dan kerja di Jakarta ga masalah. Mereka ga paham kalau rumah yang
+              digusur berarti nafkah orang di dalamnya juga digusur. Rumah bukan
+              cuma buat tidur. Social fabric orang-orang didalamnya disitu pula.
+              Rezekinya disitu.”
+            </p>
+            <p className="text-sm">
+              Mbak Evi menilai kesetiaan kelas menengah untuk tidak dan takut
+              berbicara isu-isu kelas alasannya ada di balik politik rekayasa
+              sosial floating mass sejak Orde Baru. Kelas menengah diuntungkan
+              dengan adanya kebijakan-kebijakan pembangunan serta terkena
+              mobilitas sosial lewat pendidikan yang mereka punyai. Sehingga,
+              ketika melihat penggusuran, alih-alih membacanya sebagai arena
+              konflik politis antara pemerintah, rakyat, dan moda bisnis, mereka
+              melihatnya sebagai kegagalan orang di dalamnya untuk meniti
+              kehidupan yang lebih baik.
+            </p>
           </div>
         </section>
         <BackToStart
--- src/app/focus-on/irawati/page.tsx
+++ src/app/focus-on/irawati/page.tsx
@@ -36,7 +36,7 @@ function Page() {
       <RedBar progressPercent={percent} />
       <main
         ref={containerRef}
-        className="flex overflow-x-auto overflow-y-hidden items-start flex-nowrap whitespace-nowrap h-full justify-start scrollbar-hide"
+        className="flex overflow-x-auto overflow-y-hidden items-start flex-nowrap whitespace-nowrap h-full justify-start scrollbar-hide font-zmg"
       >
         <section className="bg-black grid-system-container">
           <div className="pl-10 row-start-2 col-span-7 col-start-1 flex flex-col items-start justify-start">
@@ -78,6 +78,189 @@ function Page() {
             />
           </div>
         </section>
+        <section className="bg-black space-x-4 grid-system-container p-5 ">
+          <div className="row-start-2 col-start-1 col-span-3 flex flex-col items-start justify-start gap-5 text-justify">
+            <p className="text-sm">
+              Kak Irawati Puteri, kerap disapa Kak Ira, adalah seorang sulung
+              dari tiga bersaudara yang dibesarkan dalam keluarga sederhana dari
+              kedua orang tua yang kuat tekadnya dalam memperjuangkan
+              pendidikan. Menjadi tulang punggung keluarga sejak umur yang muda,
+              Kak Ira melakukan beragam pekerjaan untuk memenuhi kebutuhan
+              keluarganya.
+              <br />
+              <br />
+              <strong>Pendidikan, Pembuka Jalan</strong>
+              <br />
+              Berkat kegigihannya yang tidak hanya disalurkan pada pekerjaan,
+              tetapi juga pada tiap perlombaan yang ia ikuti, Kak Ira pun
+              berhasil memenangi salah satu lomba debat yang diselenggarakan
+              oleh Fakultas Hukum Universitas Indonesia sebagai best speaker
+              dalam perlombaan tersebut. Kemenangannya pun membawa dirinya untuk
+              lanjut memilih jurusan hukum sebagai jurusan yang akan ia dalami
+              pada pendidikan tingkat S1. Akan tetapi, pilihan tersebut ternyata
+              tidak didasarkan atas keinginan maupun mimpi tertentu yang hendak
+              ia capai pada saat itu. Apapun jurusannya, asalkan ia melanjutkan
+              pendidikan di jenjang lebih tinggi tidak menjadi masalah, pikirnya
+              dahulu.
+            </p>
+          </div>
+          <div className="row-start-2 col-start-5 col-span-3 flex flex-col items-start justify-start gap-5 text-justify">
+            <p className="text-sm">
+              <span className="font-junicode">
+                &quot;Begitu aku lihat apa yang mereka lakukan di sana (di dalam
+                UI), itu aku jadikan patokan aku untuk mimpi yang lebih besar
+                lagi. Jadi, menurut aku, mimpi itu bisa ikut membesar seiring
+                lingkungan kita membaik,&quot;
+              </span>
+              ucap Kak Ira.
+              <br />
+              <br />
+              Berbeda ketika Kak Ira memilih jurusan pada tingkat S1, Kak Ira
+              telah mengetahui tujuan lebih besar yang hendak ia capai ketika
+              mempertimbangkan pendidikan S2 setelah bekerja. Salah satu hal
+              yang paling mendukung adalah kehadiran tantangan dalam lingkungan
+              kerja yang multidisipliner. Hal tersebut kemudian mendorong Kak
+              Ira untuk meningkatkan pemahamannya, terutama terkait isu-isu
+              hukum dan pendidikan yang kompleks. Berkat pengalaman terdahulunya
+              dalam berpartisipasi di berbagai lomba dan kegiatan sosial
+              lainnya, hal tersebut menjadi modal yang mengantarkan Kak Ira
+              hingga diterima dalam pendidikan S2 impiannya, yaitu jurusan Law,
+              Science, and Technology di Stanford Law School.
+            </p>
+          </div>
+          <div className="row-start-2 col-start-9 col-span-3 flex flex-col items-start justify-start gap-5 text-justify">
+            <p className="text-sm">
+              <strong>Nilai Terpenting</strong>
+              <br />
+              Saat ditanyakan tentang tujuan Kak Ira kedepannya, Kak Ira tidak
+              menjawab suatu bidang spesifik, tapi justru mengenai keinginannya
+              memberi dampak.
+            </p>
+            <p className="font-junicode">
+              “Aku suka membantu orang, tapi engga aku batasain bentuknya kaya
+              gimana. Gak cuma sekadar panti asuhan, atau bahkan ngobrol kaya
+              gini. Itu yang jadi guiding value aku. I love the idea of
+              empowerment, I love the idea bahwa menjadi diri sendiri kita bisa
+              kasih pengaruh sesuai dengan apa yang ada di diri kita. Manusia
+              itu ga sekadar hitam putih, manusia pasti ada cacatnya. Jadi aku
+              suka berbicara tentang vulnerability dan gimana kita masih bisa
+              berbagi di luar dari keterbatasan.”
+            </p>
+            <p className="text-sm">
+              Ia tak merasa perlu memiliki hidup menjadi dan bertemu orang yang
+              ‘penting’, tetapi lebih utama baginya untuk menjalani hidup yang
+              sesuai nilai-nilainya.
+            </p>
+            <Image
+              src={"/images/focus-on/4.png"}
+              alt="Panti Asuhan"
+              width={243}
+              height={156}
+              className="self-start"
+            />{" "}
+          </div>
+        </section>
+        <section className="bg-black space-x-4 grid-system-container p-5">
+          <div className="row-start-2 col-start-1 col-span-3 flex flex-col items-start justify-start gap-5 text-justify">
+            <p className="text-sm">
+              <strong>Identitas Berlapis</strong>
+              <br />
+              Di dunia kerja, ada yang pernah bertanya ke Kak Ira mengapa ia
+              tidak bisa mengerjakan satu hal aja. Baginya, ia suka melihat
+              lebih dari identitas sebagai ‘lawyer’, identitas selain pekerjaan
+              utamanya.
+              <span className="font-junicode text-base">
+                “Aku ngga mau didefinisikan hanya dari pekerjaanku, tapi juga
+                banyak hal. Aku masih mau bantu-bantu di banyak hal lain.”
+              </span>
+              <br />
+              <br />
+              <strong>‘Pay It Forward’</strong>
+              <br />
+              Dari pengalamannya menjadi seorang kolaborator Beasiswa Irawati
+              Puteri dan pengusul nama di @sejutacita.id, terlihat bahwa Kak Ira
+              percaya dengan pentingnya berkontribusi kepada sesama. “Aku
+              berpikir bagaimana orang-orang merasa layak bermimpi, enggak
+              ngerasa tertinggal, berhak punya cita-cita apapun, dan berhak
+              merasa untuk mampu menggapai cita-cita tadi.” Ia ingin meneruskan
+              rezeki yang ia punya ke teman-teman dalam kondisi membutuhkan.
+              <br />
+              <br />
+              Hal ini juga dilakukannya dalam insisiatif Estafet Kebaikan.
+              Inisiatif yang mengemas
+            </p>
+          </div>
+          <div className="row-start-2 col-start-5 col-span-3 flex flex-col items-start justify-start gap-5 text-justify">
+            <p className="text-sm">
+              bantuan donasi dengan personalisasi bingkisan kado. Hal ini jauh
+              lebih menghargai penerima, sesuatu yang Kak Ira ketahui karena
+              pernah berada di sisi lain tersebut. Dengan menghindari donasi
+              dalam bentuk uang, ia juga menghindari berpartisipasi dalam rantai
+              eksploitasi.
+              <br />
+              <br />
+              <strong>Siapa yang ‘Pantas’?</strong>
+              <br />
+              Saat ditanyakan mengenai proses pemilihan ‘siapa’ yang akan
+              mendapatkan bantuan (dari sisi donasi maupun beasiswa), Kak Ira
+              mengingatkan,
+              <span className="font-junicode text-base">
+                “Jangan kita yang ukur siapa yang ‘pantas’ [secara definisi
+                moral tertentu], memberi itu memberi saja. Ada hal-hal yang
+                tidak bisa kita kontrol, jadi kita hanya bisa melakukan
+                perbuatan baik. Bisa saja praduga yang belum tentu tepat
+                menghalangi kita dari membantu orang lain. Biasanya, kalau
+                ‘resource’ terbatas kita mencoba melihat dari sisi urgensi dan
+                merit.”
+              </span>
+            </p>
+          </div>
+          <div className="row-start-2 col-start-9 col-span-3 flex flex-col items-center justify-start gap-5 text-justify">
+            <p className="text-sm">
+              <strong>Visi</strong>
+              <br />
+              Kami bertanya apa yang menurut Kak Ira dibutuhkan untuk mewujudkan
+              kemajuan di Indonesia. Sesuai dengan jurusan pilihan studi
+              lanjutnya, ia menjawab
+              <br />
+              <br />
+              <span className="font-junicode text-base">
+                “Yang menurutku paling penting adalah sumber daya manusia yang
+                membentuk ekosistem teknologinya. Teknologi itu tentang orang,
+                dan yang mendapat feedback orang juga. Sehingga, orang yang
+                membuat teknologi itu harus berkualitas. Kenapa kita tidak bisa
+                memakai ‘public service’ dari pemerintah dan sebagainya yang
+                senyaman itu [pada saat dibuat dari sektor privat] juga? Semua
+                harus berangkat dari kesadaran bahwa pemerintah juga perlu
+                membangun ekosistem yang ideal. Agar hal itu bisa terjadi, maka
+                sumber daya manusianya harus mau masuk pemerintahan juga.”
+              </span>
+            </p>
+          </div>
+        </section>
+
+        <section className="bg-black space-x-4 grid-cols-4 min-w-[33vw]  grid-rows-8 grid  gap-x-4 h-full gap-y-4 box-border whitespace-break-spaces p-5">
+          <div className="row-start-2 col-start-1 col-span-3 flex flex-col items-start justify-start gap-5 text-justify">
+            <p className="text-sm">
+              <strong>Perempuan dan Hubungan</strong>
+              <br />
+              Berbicara mengenai hubungan, Kak Ira mengungkapkan bahwa
+              pernikahan bukanlah suatu hal yang dirasa perlu untuk dipaksakan
+              urgensinya.
+              <br />
+              <br />
+              Menurutnya, menerima kondisi keluarga dengan sepenuh hati menjadi
+              salah satu aspek terpenting. Sebab, terkadang dapat kita temui
+              ketika kondisi keluarga pasangan tidak sesuai dengan apa yang
+              diharapkan dapat menimbulkan konflik karena adanya asumsi yang
+              tidak sesuai. Ia juga berpendapat bahwa kesadaran individu
+              mengenai peran gender yang mempengaruhi basis berkehidupan
+              seseorang menjadi hal penting yang perlu dipertimbangkan dalam
+              mencari pasangan. 
+            </p>
+          </div>
+        </section>
+
         <BackToStart
           onBack={() => {
             resetSavedScroll();
